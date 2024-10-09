12b88075b45a92c7012da0d9ff591537d48e535a
nahuelbenitz <bnahuel83@gmail.com>
2024-10-09 11:37:11 -0300

    Add project files.

---
+++ Peliculas.API/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Peliculas.API.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; } = null!;
+        [Required]
+        [DisplayName("Fecha de creación")]
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
