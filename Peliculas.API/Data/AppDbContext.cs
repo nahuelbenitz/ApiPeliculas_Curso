54ddef0064dd021da5160504aaae145087762002
nahuelbenitz <bnahuel83@gmail.com>
2024-10-09 12:00:22 -0300

    primera migracion

---
+++ Peliculas.API/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Peliculas.API.Models;
+
+namespace Peliculas.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>().ToTable("Categoria");
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
--- Peliculas.API/Program.cs
+++ Peliculas.API/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Peliculas.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Contexto de datos
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
 
 var app = builder.Build();
 
